f110b3547c540cb67a7cac6b61dc52712f9431a6
Benecia Shi <Benecia@shannons-macbook-air.local>
2018-07-11 11:09:22 -0500

    Added Mount Rushmore landmark location

--- Mobile Mapper/ViewController.swift
+++ Mobile Mapper/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 import MapKit
+import CoreLocation  //have to put asking permission to use location in Info.plist/ plist
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    
     let northwesternAnnotation = MKPointAnnotation()
+    let locationManager = CLLocationManager()
+    let address = "Mount Rushmore"
+    let geocoder = CLGeocoder()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,13 +27,16 @@ class ViewController: UIViewController {
         northwesternAnnotation.coordinate = coordinate
         northwesternAnnotation.title = "Northwestern University"
         mapView.addAnnotation(northwesternAnnotation) // pin on Northwestern location
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true //default is location of Apple's headquarters in Cupertino California, shows default cuz there's no GPS system in laptop as in phone, so simulate location by going to Debug (on top bar) -> Simulate location -> Custom -> 47.0516, -87.6814 ???
+        geocoder.geocodeAddressString(address) { (placemarks, error) in //Mount Rushmore location
+            for placemark in placemarks! {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = (placemark.location?.coordinate)!
+                annotation.title = placemark.name
+                self.mapView.addAnnotation(annotation)
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
